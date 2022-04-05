bce2e3062a5510c3e83826c0e108c012cf798f35
Elizabeth Ptack <101746864+EPtackArts@users.noreply.github.com>
2022-04-05 18:37:08 -0500

    The rest
    the questions

--- index.js
+++ index.js
@@ -1,6 +1,64 @@
 response = window.prompt("Hello!")
 if(response === "I am Ethan Hawke"){
     window-prompt("Nice to meet you!")
+    else { 
+        window-prompt("That's it bye.")
+    }
 } else {
     window-prompt("Are you Ethan Hawke?")
+    if(response === "yes"){
+        window-prompt("Nice to meet you!")
+        else { 
+            window-prompt("That's it bye.")
+        }
+    } else if(response === "no"){
+        window-prompt("Are you sure")
+        if(response === "yes"){
+            window-prompt("How do you know you're sure?")
+            else {
+                window-promt("Hate to break it to you, you are Ethan Hawke.")
+                else { 
+                    window-prompt("That's it bye.")
+                }
+            }
+        } else if(response === "no"){
+            window-prompt("See, you ARE Ethan Hawke!")
+            else { 
+                window-prompt("That's it bye.")
+            }
+        } else {
+            window-prompt("You're probably Ethan Hawke.")
+            else { 
+                window-prompt("That's it bye.")
+            }
+        }
+    } else {
+        window-prompt("It's a yes or no question. Are you Ethan Hawke? yes or no?")
+    if(response === "yes"){
+        window-prompt("Nice to meet you!")
+        else { 
+            window-prompt("That's it bye.")
+        }
+    } else if(response === "no"){
+        window-prompt("Are you sure")
+        if(response === "yes"){
+            window-prompt("How do you know you're sure?")
+            else {
+                window-promt("Hate to break it to you, you are Ethan Hawke.")
+                else { 
+                    window-prompt("That's it bye.")
+                }
+            }
+        } else if(response === "no"){
+            window-prompt("See, you ARE Ethan Hawke!")
+            else { 
+                window-prompt("That's it bye.")
+            }
+        } else {
+            window-prompt("You're probably Ethan Hawke.")
+            else { 
+                window-prompt("That's it bye.")
+            }
+        }
+    }
 }
